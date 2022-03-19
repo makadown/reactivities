0366b035200aba7300fc2af9fdbb7a973dece4d2
makadown <maseflores79@gmail.com>
2022-03-19 15:13:44 -0500

    Completing walking skeleton

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [activities, setActivites] = useState([]);
+
+  useEffect(() => {
+    // first
+    axios.get('http://localhost:5000/api/activities')
+      .then( response => {
+        console.log(response);
+        setActivites(response.data);
+      });
+    /*return () => {
+      second
+    }*/
+  }, [/*third*/]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header as='h2' icon='users' content="Reactivities"/>
+        <List>
+          {activities.map( activity => (
+            <List.Item key={activity.id}>
+              { activity.title }
+            </List.Item>
+          ))}
+        </List>
     </div>
   );
 }
